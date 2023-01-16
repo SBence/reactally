c36898f50a18c3790f5a73b3f42b8a5ca770cb69
SBence <43538202+SBence@users.noreply.github.com>
2023-01-16 14:21:05 +0100

    Add confirmation modal for removing all counters

--- src/components/MenuButton.tsx
+++ src/components/MenuButton.tsx
@@ -8,6 +8,7 @@ import {
 } from "@tabler/icons";
 import { Dispatch, SetStateAction, useState } from "react";
 import { Counters } from "../interfaces/counter";
+import RemoveModal from "./RemoveModal";
 
 export default ({
   counters,
@@ -18,110 +19,115 @@ export default ({
 }) => {
   const theme = useMantineTheme();
 
-  const [opened, setOpened] = useState(false);
+  const [menuOpened, setMenuOpened] = useState(false);
+  const [removeModalOpened, setRemoveModalOpened] = useState(false);
   const [loading, setLoading] = useState(false);
 
   return (
-    <Menu
-      opened={opened}
-      onChange={setOpened}
-      transition="scale-y"
-      position="top-end"
-      width={160}
-      closeOnItemClick={false}
-      withinPortal
-    >
-      <Menu.Target>
-        <Button
-          color="teal.4"
-          variant="outline"
-          rightIcon={<IconChevronDown size={18} stroke={1.5} />}
-          pr={12}
-          loading={loading}
-          loaderPosition="right"
-        >
-          Options
-        </Button>
-      </Menu.Target>
-      <Menu.Dropdown>
-        <Menu.Label>Backup</Menu.Label>
-        <FileButton
-          onChange={async (uploadedFile) => {
-            setLoading(true);
-            const uploadedText = await uploadedFile?.text();
-            if (!uploadedText)
-              return showNotification({
-                title: "Failed to import file.",
-                message: "Non-text file uploaded.",
-                color: "red",
-              });
-            try {
-              const uploadedJson = JSON.parse(uploadedText);
-              setCounters((oldCounters) => {
-                return { ...oldCounters, ...uploadedJson };
-              });
-              showNotification({
-                message: "Successfully imported file.",
-                color: "green",
-              });
-            } catch {
-              showNotification({
-                title: "Failed to import file.",
-                message: "Invalid JSON file uploaded.",
-                color: "red",
-              });
-            }
-            setOpened(false);
-            setLoading(false);
-          }}
-          accept="application/json"
-        >
-          {(props) => (
-            <Menu.Item
-              icon={
-                <IconDatabaseImport
-                  size={16}
-                  color={theme.colors.teal[4]}
-                  stroke={1.5}
-                />
+    <>
+      <RemoveModal
+        opened={removeModalOpened}
+        setOpened={setRemoveModalOpened}
+        setCounters={setCounters}
+      />
+      <Menu
+        opened={menuOpened}
+        onChange={setMenuOpened}
+        transition="scale-y"
+        position="top-end"
+        width={160}
+        closeOnItemClick={false}
+        withinPortal
+      >
+        <Menu.Target>
+          <Button
+            color="teal.4"
+            variant="outline"
+            rightIcon={<IconChevronDown size={18} stroke={1.5} />}
+            pr={12}
+            loading={loading}
+            loaderPosition="right"
+          >
+            Options
+          </Button>
+        </Menu.Target>
+        <Menu.Dropdown>
+          <Menu.Label>Backup</Menu.Label>
+          <FileButton
+            onChange={async (uploadedFile) => {
+              setLoading(true);
+              const uploadedText = await uploadedFile?.text();
+              if (!uploadedText)
+                return showNotification({
+                  title: "Failed to import file.",
+                  message: "Non-text file uploaded.",
+                  color: "red",
+                });
+              try {
+                const uploadedJson = JSON.parse(uploadedText);
+                setCounters((oldCounters) => {
+                  return { ...oldCounters, ...uploadedJson };
+                });
+                showNotification({
+                  message: "Successfully imported file.",
+                  color: "green",
+                });
+              } catch {
+                showNotification({
+                  title: "Failed to import file.",
+                  message: "Invalid JSON file uploaded.",
+                  color: "red",
+                });
               }
-              {...props}
-            >
-              Import
-            </Menu.Item>
-          )}
-        </FileButton>
+              setMenuOpened(false);
+              setLoading(false);
+            }}
+            accept="application/json"
+          >
+            {(props) => (
+              <Menu.Item
+                icon={
+                  <IconDatabaseImport
+                    size={16}
+                    color={theme.colors.teal[4]}
+                    stroke={1.5}
+                  />
+                }
+                {...props}
+              >
+                Import
+              </Menu.Item>
+            )}
+          </FileButton>
 
-        <Menu.Item
-          component="a"
-          href={"data:text/json;charset=utf-8," + JSON.stringify(counters)}
-          download="ReacTally.json"
-          onClick={() => setOpened(false)}
-          icon={
-            <IconDatabaseExport
-              size={16}
-              color={theme.colors.teal[4]}
-              stroke={1.5}
-            />
-          }
-        >
-          Export
-        </Menu.Item>
-        <Menu.Divider />
-        <Menu.Item
-          disabled={!Object.keys(counters).length}
-          onClick={() => {
-            setOpened(false);
-            setCounters({});
-            showNotification({
-              message: "Removed all counters.",
-            });
-          }}
-          icon={<IconX size={16} color={theme.colors.red[5]} stroke={1.5} />}
-        >
-          Remove all
-        </Menu.Item>
-      </Menu.Dropdown>
-    </Menu>
+          <Menu.Item
+            component="a"
+            href={"data:text/json;charset=utf-8," + JSON.stringify(counters)}
+            download="ReacTally.json"
+            onClick={() => setMenuOpened(false)}
+            icon={
+              <IconDatabaseExport
+                size={16}
+                color={theme.colors.teal[4]}
+                stroke={1.5}
+              />
+            }
+          >
+            Export
+          </Menu.Item>
+          <Menu.Divider />
+          <Menu.Item
+            disabled={!Object.keys(counters).length}
+            onClick={() => {
+              setRemoveModalOpened(true);
+              setMenuOpened(false);
+            }}
+            icon={<IconX size={16} color={theme.colors.red[5]} stroke={1.5} />}
+          >
+            Remove all
+          </Menu.Item>
+        </Menu.Dropdown>
+      </Menu>
+    </>
   );
 };
---
+++ src/components/RemoveModal.tsx
@@ -0,0 +1,51 @@
+import { Button, Group, Modal, Space, ThemeIcon, Title } from "@mantine/core";
+import { showNotification } from "@mantine/notifications";
+import { IconTrash } from "@tabler/icons";
+import { Dispatch, SetStateAction } from "react";
+import { Counters } from "../interfaces/counter";
+
+export default ({
+  opened,
+  setOpened,
+  setCounters,
+}: {
+  opened: boolean;
+  setOpened: Dispatch<SetStateAction<boolean>>;
+  setCounters: Dispatch<SetStateAction<Counters>>;
+}) => {
+  return (
+    <Modal
+      opened={opened}
+      onClose={() => setOpened(false)}
+      size="auto"
+      withCloseButton={false}
+    >
+      <Group>
+        <ThemeIcon variant="light" size="xl" radius="xl" color="red">
+          <IconTrash size={24} />
+        </ThemeIcon>
+        <Title order={4} align="center">
+          Remove all counters?
+        </Title>
+      </Group>
+      <Space h="xl" />
+      <Group position="right">
+        <Button color="gray" onClick={() => setOpened(false)}>
+          Cancel
+        </Button>
+        <Button
+          color="red"
+          onClick={() => {
+            setOpened(false);
+            setCounters({});
+            showNotification({
+              message: "Removed all counters.",
+            });
+          }}
+        >
+          Remove
+        </Button>
+      </Group>
+    </Modal>
+  );
+};
