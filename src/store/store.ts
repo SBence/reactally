1ea5fbd7ab37e062b26cc2c05318c8a8550d9035
SBence <43538202+SBence@users.noreply.github.com>
2023-01-20 10:40:04 +0100

    Save Redux state into LocalStorage

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,12 +2,19 @@ import { configureStore } from "@reduxjs/toolkit";
 import accentColorSlice from "./slices/accentColorSlice";
 import countersSlice from "./slices/countersSlice";
 
+const savedState = localStorage.getItem("state");
+
 export const store = configureStore({
   reducer: {
     accentColor: accentColorSlice,
     counters: countersSlice,
   },
+  preloadedState: savedState ? JSON.parse(savedState) : undefined,
 });
+
+store.subscribe(() =>
+  localStorage.setItem("state", JSON.stringify(store.getState()))
+);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
