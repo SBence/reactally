07de4331d2fa8689d689ba8984c2a832888c024f
SBence <43538202+SBence@users.noreply.github.com>
2023-09-29 19:41:18 +0200

    Add Open Graph metadata

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { lingui } from "@lingui/vite-plugin";
+import { createHtmlPlugin } from "vite-plugin-html";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,5 +12,12 @@ export default defineConfig({
       },
     }),
     lingui(),
+    createHtmlPlugin({
+      inject: {
+        data: {
+          publicUrl: process.env.PUBLIC_URL,
+        },
+      },
+    }),
   ],
 });
