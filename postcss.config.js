4eb764d9fb80e41a118d0328059b6264087ec41a
SBence <43538202+SBence@users.noreply.github.com>
2024-10-20 20:05:20 +0200

    Project configuration updates
    - Add *.tsbuildinfo to gitignore
    - Add Lingui ESLint plugin
    - Lock @tabler/icons-react to ~3.18.0, ignore 3.19.0 with Dependabot
    - Use stable version (5.0.0) of eslint-plugin-react-hooks
    - Use em instead of px in PostCSS configuration
    - Format code to Prettier style
    - Update dependencies

--- postcss.config.js
+++ postcss.config.js
@@ -3,11 +3,11 @@ export default {
     "postcss-preset-mantine": {},
     "postcss-simple-vars": {
       variables: {
-        "mantine-breakpoint-xs": "680px",
-        "mantine-breakpoint-sm": "980px",
-        "mantine-breakpoint-md": "1300px",
-        "mantine-breakpoint-lg": "1620px",
-        "mantine-breakpoint-xl": "1940px",
+        "mantine-breakpoint-xs": "36em",
+        "mantine-breakpoint-sm": "48em",
+        "mantine-breakpoint-md": "62em",
+        "mantine-breakpoint-lg": "75em",
+        "mantine-breakpoint-xl": "88em",
       },
     },
   },
